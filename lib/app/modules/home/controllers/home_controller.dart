ed6077136dd2d31d83c1cfc9757f9b7a0d9f2415
高江华 <g598670138@163.com>
2024-03-08 17:50:04 +0800

    更新

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,39 @@
+/*
+ * @Author: 高江华 g598670138@163.com
+ * @Date: 2024-03-08 11:18:46
+ * @LastEditors: 高江华
+ * @LastEditTime: 2024-03-08 17:41:40
+ * @Description: file content
+ */
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  RxBool flag = false.obs;
+  final ScrollController scrollController = ScrollController();
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels > 10 &&
+          scrollController.position.pixels < 20) {
+        if (!flag.value) {
+          flag.value = true;
+          update();
+        }
+      }
+      if (scrollController.position.pixels < 10) {
+        if (flag.value) {
+          flag.value = false;
+          update();
+        }
+      }
+    });
   }
 
   @override
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,33 +1,108 @@
-/*
- * @Author: 高江华 g598670138@163.com
- * @Date: 2024-03-08 11:18:46
- * @LastEditors: 高江华
- * @LastEditTime: 2024-03-08 16:05:21
- * @Description: file content
- */
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
+import '../../../services/keepAliveWrapper.dart';
+
+import "../../../services/screenAdapter.dart";
+
+import "../../../services/ityingFonts.dart";
+
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        elevation: 0,
+    return KeepAliveWrapper(
+        child: Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+              top: -60,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: ListView.builder(
+                  controller: controller.scrollController, //绑定controller
+                  itemCount: 20,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return SizedBox(
+                        width: ScreenAdapter.width(1080),
+                        height: ScreenAdapter.height(682),
+                        child: Image.network(
+                          "https://www.itying.com/images/focus/focus02.png",
+                          fit: BoxFit.cover,
+                        ),
+                      );
+                    } else {
+                      return ListTile(title: Text("第$index个列表"));
+                    }
+                  })),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Obx(() => AppBar(
+                  leading: controller.flag.value
+                      ? const Text("")
+                      : const Icon(
+                          ItyingFonts.xiaomi,
+                          color: Colors.white,
+                        ),
+                  leadingWidth: controller.flag.value
+                      ? ScreenAdapter.width(40)
+                      : ScreenAdapter.width(140),
+                  title: AnimatedContainer(
+                    width: controller.flag.value
+                        ? ScreenAdapter.width(800)
+                        : ScreenAdapter.width(620),
+                    height: ScreenAdapter.height(96),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(230, 252, 243, 236),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    duration: const Duration(milliseconds: 600),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(ScreenAdapter.width(34),
+                              0, ScreenAdapter.width(10), 0),
+                          child: const Icon(Icons.search),
+                        ),
+                        Text("手机",
+                            style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: ScreenAdapter.fontSize(32)))
+                      ],
+                    ),
+                  ),
+                  centerTitle: true,
+                  backgroundColor:
+                      controller.flag.value ? Colors.white : Colors.transparent,
+                  elevation: 0,
+                  actions: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.qr_code,
+                          color: controller.flag.value
+                              ? Colors.black87
+                              : Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.message,
+                            color: controller.flag.value
+                                ? Colors.black87
+                                : Colors.white))
+                  ],
+                )),
+          )
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    ));
   }
 }
