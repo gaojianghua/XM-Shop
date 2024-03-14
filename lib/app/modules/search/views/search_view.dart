e9452d1adddb2bd185d00db2f708ab2f8700004d
高江华 <g598670138@163.com>
2024-03-14 22:18:21 +0800

    更新

--- lib/app/modules/search/views/search_view.dart
+++ lib/app/modules/search/views/search_view.dart
@@ -2,10 +2,12 @@
  * @Author: 高江华 g598670138@163.com
  * @Date: 2024-03-14 16:14:42
  * @LastEditors: 高江华
- * @LastEditTime: 2024-03-14 17:44:26
+ * @LastEditTime: 2024-03-14 22:16:47
  * @Description: file content
  */
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 import 'package:xmshop/app/services/screenAdapter.dart';
@@ -30,13 +32,11 @@ class SearchView extends GetView<SearchsController> {
               cursorHeight: ScreenAdapter.height(46),
               autofocus: true,
               decoration: InputDecoration(
-                contentPadding: const EdgeInsets.all(0),
-                prefixIcon: const Icon(Icons.search, color: Colors.black54),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide.none
-                )
-              ),
+                  contentPadding: const EdgeInsets.all(0),
+                  prefixIcon: const Icon(Icons.search, color: Colors.black54),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none)),
             ),
           ),
           centerTitle: true,
@@ -44,39 +44,129 @@ class SearchView extends GetView<SearchsController> {
           elevation: 0,
           actions: [
             TextButton(
-                onPressed: () {}, child: const Text("搜索", style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black
-                )))
+                onPressed: () {},
+                child: const Text("搜索",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500, color: Colors.black)))
           ]),
       body: ListView(
         padding: EdgeInsets.all(ScreenAdapter.width(20)),
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "搜索历史",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: ScreenAdapter.fontSize(42)
-                ),
-              ),
-              const Icon(Icons.delete_forever_outlined)
-            ],
-          ),
+          Padding(
+              padding: EdgeInsets.only(bottom: ScreenAdapter.height(20)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "搜索历史",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenAdapter.fontSize(42)),
+                  ),
+                  const Icon(Icons.delete_forever_outlined)
+                ],
+              )),
           Wrap(
             children: [
               Container(
-                padding: EdgeInsets.all(ScreenAdapter.height(16)),
+                padding: EdgeInsets.fromLTRB(
+                    ScreenAdapter.width(32),
+                    ScreenAdapter.width(16),
+                    ScreenAdapter.width(32),
+                    ScreenAdapter.width(16)),
                 margin: EdgeInsets.all(ScreenAdapter.height(16)),
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10)
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
                 child: const Text("手机"),
               )
             ],
+          ),
+          SizedBox(
+            height: ScreenAdapter.height(50),
+          ),
+          Padding(
+              padding: EdgeInsets.only(bottom: ScreenAdapter.height(20)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "猜你想搜",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: ScreenAdapter.fontSize(42)),
+                  ),
+                  const Icon(Icons.refresh)
+                ],
+              )),
+          Wrap(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(
+                    ScreenAdapter.width(32),
+                    ScreenAdapter.width(16),
+                    ScreenAdapter.width(32),
+                    ScreenAdapter.width(16)),
+                margin: EdgeInsets.all(ScreenAdapter.height(16)),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Text("手机"),
+              )
+            ],
+          ),
+          SizedBox(
+            height: ScreenAdapter.height(50),
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.white),
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: ScreenAdapter.height(138),
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/hot_search.png"))),
+                ),
+                Container(
+                  padding: EdgeInsets.all(ScreenAdapter.width(20)),
+                  child: GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: 8,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: ScreenAdapter.width(40),
+                          mainAxisSpacing: ScreenAdapter.height(20),
+                          childAspectRatio: 3 / 1),
+                      itemBuilder: ((context, index) {
+                        return Row(
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              width: ScreenAdapter.width(120),
+                              padding: EdgeInsets.all(ScreenAdapter.width(10)),
+                              child: Image.network(
+                                "",
+                                fit: BoxFit.fitHeight,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                alignment: Alignment.topLeft,
+                                padding:
+                                    EdgeInsets.all(ScreenAdapter.width(10)),
+                                child: Text(""),
+                              ),
+                            )
+                          ],
+                        );
+                      })),
+                )
+              ],
+            ),
           )
         ],
       ),
