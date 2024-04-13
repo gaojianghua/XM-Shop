1584855837b6a48a937dbc5b2c7535a068bdd4ae
高江华 <g598670138@163.com>
2024-04-13 18:23:52 +0800

    更新

--- lib/app/modules/address/addressAdd/views/address_add_view.dart
+++ lib/app/modules/address/addressAdd/views/address_add_view.dart
@@ -2,7 +2,7 @@
  * @Author: 高江华 g598670138@163.com
  * @Date: 2024-04-11 14:56:28
  * @LastEditors: 高江华
- * @LastEditTime: 2024-04-11 16:42:50
+ * @LastEditTime: 2024-04-13 18:23:07
  * @Description: file content
  */
 import 'package:flutter/material.dart';
@@ -77,6 +77,82 @@ class AddressAddView extends GetView<AddressAddController> {
                       ],
                     )
                   ],
+                )),
+            SizedBox(
+              height: ScreenAdapter.height(40),
+            ),
+            Container(
+                padding: EdgeInsets.all(ScreenAdapter.width(20)),
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "所在地区",
+                          style:
+                              TextStyle(fontSize: ScreenAdapter.fontSize(46)),
+                        ),
+                        SizedBox(
+                          width: ScreenAdapter.width(30),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  helperStyle: TextStyle(
+                                      fontSize: ScreenAdapter.fontSize(46)),
+                                  hintText: "请输入所在地址",
+                                  border: InputBorder.none),
+                            ))
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "详细地址",
+                          style:
+                              TextStyle(fontSize: ScreenAdapter.fontSize(46)),
+                        ),
+                        SizedBox(
+                          width: ScreenAdapter.width(30),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  helperStyle: TextStyle(
+                                      fontSize: ScreenAdapter.fontSize(46)),
+                                  hintText: "请输入详细地址",
+                                  border: InputBorder.none),
+                            ))
+                      ],
+                    )
+                  ],
+                )),
+            SizedBox(
+              height: ScreenAdapter.height(200),
+            ),
+            Padding(
+                padding: EdgeInsets.all(ScreenAdapter.width(20)),
+                child: InkWell(
+                  onTap: () {
+                    
+                  },
+                  child: Container(
+                    height: ScreenAdapter.height(140),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(251, 72, 5, 0.9),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Text(
+                      "保存",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
                 ))
           ],
         ));
